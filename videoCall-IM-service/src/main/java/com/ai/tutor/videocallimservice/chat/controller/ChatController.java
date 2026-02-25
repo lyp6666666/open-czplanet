638f457652e0d3c84097d1a540d77905f96cd893
Lihongwei2005923 <3232539921@qq.com>
2026-02-25 10:39:38 +0800

    添加聊天限制

--- videoCall-IM-service/src/main/java/com/ai/tutor/videocallimservice/chat/controller/ChatController.java
+++ videoCall-IM-service/src/main/java/com/ai/tutor/videocallimservice/chat/controller/ChatController.java
@@ -2,12 +2,18 @@ package com.ai.tutor.videocallimservice.chat.controller;
 
 
 import com.ai.tutor.common.BaseResponse;
+import com.ai.tutor.enums.ErrorCode;
 import com.ai.tutor.utils.RequestHolder;
 import com.ai.tutor.utils.ResultUtils;
+import com.ai.tutor.utils.ThrowUtils;
+import com.ai.tutor.videocallimservice.chat.domain.entity.CollaborationProposal;
+import com.ai.tutor.videocallimservice.chat.domain.enums.CollaborationProposalStatus;
 import com.ai.tutor.videocallimservice.chat.domain.vo.request.ChatMessagePageReq;
 import com.ai.tutor.videocallimservice.chat.domain.vo.request.ChatMessageReq;
 import com.ai.tutor.videocallimservice.chat.domain.vo.response.ChatMessageResp;
 import com.ai.tutor.videocallimservice.chat.domain.vo.response.CursorPageBaseResp;
+import com.ai.tutor.videocallimservice.chat.mapper.CollaborationProposalMapper;
+import com.ai.tutor.videocallimservice.chat.mapper.RoomMapper;
 import com.ai.tutor.videocallimservice.chat.service.ChatService;
 import com.ai.tutor.common.annotation.FrequencyControl;
 import io.swagger.v3.oas.annotations.Operation;
@@ -26,6 +32,9 @@ public class ChatController {
     @Resource
     private ChatService chatService;
 
+    @Resource
+    private CollaborationProposalMapper collaborationProposalMapper;
+
     @GetMapping("/public/msg/page")
     @Operation(summary = "消息列表")
     @FrequencyControl(time = 120, count = 20, target = FrequencyControl.Target.IP)
@@ -42,10 +51,20 @@ public class ChatController {
     @FrequencyControl(time = 30, count = 5, target = FrequencyControl.Target.UID)
     @FrequencyControl(time = 60, count = 10, target = FrequencyControl.Target.UID)
     public BaseResponse<ChatMessageResp> sendMsg(@Valid @RequestBody ChatMessageReq request) {
+        //增加聊天设置要求合作后才能发送消息的校验，避免用户绕过前端限制直接调用接口发送消息
+        ThrowUtils.throwIf(preHandler(request),new RuntimeException("请先与对方达成合作协议，再尝试发送消息"));
+
         Long msgId = chatService.sendMsg(request, RequestHolder.get().getUid());
         //返回完整消息格式，方便前端展示
         return ResultUtils.success(chatService.getMsgResp(msgId, RequestHolder.get().getUid()));
 
+    }
+
+    private boolean preHandler(ChatMessageReq request) {
+        Long roomId = request.getRoomId();
+        CollaborationProposal collaborationProposal = collaborationProposalMapper.selectLatestByRoomId(roomId);
+        return collaborationProposal != null &&
+                collaborationProposal.getStatus().equals(CollaborationProposalStatus.ACCEPTED.name());
     }
 
 }
